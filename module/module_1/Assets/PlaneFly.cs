a67ab743ebfc1303ea28e186928b3dc41dba6fa7
telingerErhard <93592012+tErhard@users.noreply.github.com>
2024-03-27 08:23:31 +0200

    Update PlaneFly.cs

--- module/module_1/Assets/PlaneFly.cs
+++ module/module_1/Assets/PlaneFly.cs
@@ -2,23 +2,18 @@ using UnityEngine;
 
 public class CircleMovement : MonoBehaviour
 {
-    public float radius = 5f; // Радіус кола
-    public float speed = 2f; // Швидкість руху по колу
-    private float angle = 0f; // Кут, на який об'єкт повернутий
-
+    public float radius = 5f;
+    public float speed = 2f;
+    private float angle = 0f;
     void Update()
     {
-        // Обчислюємо нові координати об'єкта на колі
         float x = Mathf.Cos(angle) * radius;
         float z = Mathf.Sin(angle) * radius;
 
-        // Встановлюємо нові координати об'єкта
         transform.position = new Vector3(x, 5f, z);
 
-        // Збільшуємо кут для наступного кадру
         angle += speed * Time.deltaTime;
 
-        // Якщо кут стає більшим за 360 градусів, то повертаємо його назад
         if (angle >= 360f)
         {
             angle -= 360f;
