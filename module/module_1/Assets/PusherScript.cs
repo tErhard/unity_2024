c59fe83921fd7a15c152d736717c5e71cd5fcbb3
telingerErhard <93592012+tErhard@users.noreply.github.com>
2024-03-27 08:22:27 +0200

    Update PusherScript.cs

--- module/module_1/Assets/PusherScript.cs
+++ module/module_1/Assets/PusherScript.cs
@@ -2,25 +2,20 @@ using UnityEngine;
 
 public class ObjectController : MonoBehaviour
 {
-    public float fallSpeed = 5f; // Швидкість падіння об'єкту
-    private bool isFalling = false; // Прапорець, який показує, чи об'єкт падає
+    public float fallSpeed = 5f;
+    private bool isFalling = false;
 
     void Update()
     {
-        // Перевірка, чи натиснуто клавішу пробілу і об'єкт не падає вже
         if (Input.GetKeyDown(KeyCode.Space) && !isFalling)
         {
-            isFalling = true; // Встановлення прапорця, що об'єкт падає
+            isFalling = true;
         }
 
-        // Якщо об'єкт падає, змінюємо його позицію по осі Y
         if (isFalling)
         {
-            // Визначаємо нову позицію об'єкту
             Vector3 newPosition = transform.position;
-            newPosition.y -= fallSpeed * Time.deltaTime; // Рухаємо об'єкт вниз зі швидкістю fallSpeed
-
-            // Присвоюємо нову позицію об'єкту
+            newPosition.y -= fallSpeed * Time.deltaTime;
             transform.position = newPosition;
         }
     }
